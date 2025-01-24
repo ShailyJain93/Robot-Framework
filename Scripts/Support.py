b4dc17bd900f2427e4fae8d5c450f4fa7b6243bd
ShailyJain93 <sjshaily07@gmail.com>
2025-01-24 18:02:28 +0530

    Third commit

--- Scripts/Support.py
+++ Scripts/Support.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 def read_xml(node_name):
     # parse an xml file by name
-    my_doc = minidom.parse('D:/New Framework/Global.xml')
-    #my_doc = minidom.parse( str(Path(__file__).parent.parent)+'/dataTable.xml')
+    my_doc = minidom.parse(str(Path(__file__).parent.parent)+'/Global.xml')
     data = my_doc.getElementsByTagName(node_name)[0]
     return data.firstChild.data
